5059d27953d63d2fea6270cfbc70bb82148a8eb0
taichi <taicloud0803@icloud.com>
2021-12-17 14:24:31 +0900

    new groupを作成

---
+++ PostLike/View/Cells/SearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchTableViewCell.swift
+//  PostLike
+//
+//  Created by taichi on 2021/12/17.
+//  Copyright © 2021 taichi. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
