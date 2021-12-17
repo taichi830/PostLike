780b0d8ac3408b8ca3b8cdfd6866c689a5b1e561
taichi <taicloud0803@icloud.com>
2021-12-18 01:19:44 +0900

    labelを共通化

--- PostLike/CustomClass/MessageLabel.swift
+++ PostLike/CustomClass/MessageLabel.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2021 taichi. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class MessageLabel: UILabel{
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func setupLabel(view:UIView,y:CGFloat){
+        self.frame = CGRect(x: 0, y: y, width: view.frame.size.width, height: 30)
+        self.textColor = .lightGray
+        self.textAlignment = .center
+        self.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+    }
+    
+    
+    
+    
+    
+}
