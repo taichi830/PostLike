1483a2d6454d32ddb2280c421a94665da3b2d68e
taichi <taicloud0803@icloud.com>
2021-12-24 23:39:43 +0900

    Firebase Extensionを作成

---
+++ PostLike/Firebase/Auth.swift
@@ -0,0 +1,9 @@
+//
+//  Auth.swift
+//  PostLike
+//
+//  Created by taichi on 2021/12/24.
+//  Copyright © 2021 taichi. All rights reserved.
+//
+
+import Foundation
---
+++ PostLike/Firebase/Storage.swift
@@ -0,0 +1,9 @@
+//
+//  Storage.swift
+//  PostLike
+//
+//  Created by taichi on 2021/12/22.
+//  Copyright © 2021 taichi. All rights reserved.
+//
+
+import Foundation
