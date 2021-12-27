7dbf427f382eb8dd6adca22c51faf5222c171f52
taichi <taicloud0803@icloud.com>
2021-12-27 22:49:32 +0900

    Custom UIViewを作成

---
+++ PostLike/CustomClass/FlexibleHeightImageView.swift
@@ -0,0 +1,21 @@
+//
+//  FlexibleHeightImageView.swift
+//  PostLike
+//
+//  Created by taichi on 2021/12/26.
+//  Copyright © 2021 taichi. All rights reserved.
+//
+
+import UIKit
+
+class FlexibleHeightImageView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
