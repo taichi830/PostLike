ea495c076ac63e4706c1b7c6c7607c8e1e5129c4
taichi <taicloud0803@icloud.com>
2022-01-18 02:02:22 +0900

    apple認証機能を実装

--- PostLike/ViewController/Login/InitialViewController.swift
+++ PostLike/ViewController/Login/InitialViewController.swift
@@ -9,13 +9,20 @@
 import UIKit
 import FirebaseFirestore
 import FirebaseAuth
+import AuthenticationServices
+import CryptoKit
 
 final class InitialViewController: UIViewController {
 
     
     @IBOutlet private weak var backView: UIView!
+    
+    @IBOutlet weak var registerWithAppleButton: UIButton!
+    
     @IBOutlet private weak var createAccountButton: UIButton!
+    
     @IBOutlet private weak var loginButton: UIButton!
+    
     @IBOutlet private weak var agreeTextView: UITextView!{
         didSet{
             agreeTextView.isScrollEnabled = false
@@ -39,22 +46,34 @@ final class InitialViewController: UIViewController {
         }
     }
     
+    fileprivate var currentNonce: String?
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        createAccountButton.layer.cornerRadius = 28
+        
+        registerWithAppleButton.layer.cornerRadius = 27
+        registerWithAppleButton.layer.shadowColor = UIColor.black.cgColor
+        registerWithAppleButton.layer.shadowRadius = 4
+        registerWithAppleButton.layer.shadowOffset = CGSize(width: 0, height: 1)
+        registerWithAppleButton.layer.shadowOpacity = 0.2
+        
+        createAccountButton.layer.cornerRadius = 27
         createAccountButton.layer.shadowColor = UIColor.black.cgColor
         createAccountButton.layer.shadowRadius = 4
         createAccountButton.layer.shadowOffset = CGSize(width: 0, height: 1)
-        createAccountButton.layer.shadowOpacity = 0.3
+        createAccountButton.layer.shadowOpacity = 0.2
         
-        loginButton.layer.cornerRadius = 30
+        loginButton.layer.cornerRadius = 27
         loginButton.layer.shadowColor = UIColor.black.cgColor
         loginButton.layer.shadowRadius = 4
         loginButton.layer.shadowOffset = CGSize(width: 0, height: 1)
         loginButton.layer.shadowOpacity = 0.2
+        
+        
+        
         
         if Auth.auth().currentUser?.isEmailVerified == false{
             let confirmVC = self.storyboard?.instantiateViewController(withIdentifier: "confirm") as! ComfirmEmailViewController
@@ -63,6 +82,11 @@ final class InitialViewController: UIViewController {
         
         
         
+    }
+    
+    
+    @IBAction func didTapRegisterWithAppleButton(_ sender: Any) {
+        startSignInWithAppleFlow()
     }
     
     
@@ -79,6 +103,124 @@ final class InitialViewController: UIViewController {
     
 
 }
+
+
+extension InitialViewController: ASAuthorizationControllerDelegate,ASAuthorizationControllerPresentationContextProviding {
+    
+    private func randomNonceString(length: Int = 32) -> String {
+      precondition(length > 0)
+      let charset: [Character] =
+        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+      var result = ""
+      var remainingLength = length
+
+      while remainingLength > 0 {
+        let randoms: [UInt8] = (0 ..< 16).map { _ in
+          var random: UInt8 = 0
+          let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+          if errorCode != errSecSuccess {
+            fatalError(
+              "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
+            )
+          }
+          return random
+        }
+
+        randoms.forEach { random in
+          if remainingLength == 0 {
+            return
+          }
+
+          if random < charset.count {
+            result.append(charset[Int(random)])
+            remainingLength -= 1
+          }
+        }
+      }
+
+      return result
+    }
+    
+    
+    @available(iOS 13, *)
+    private func sha256(_ input: String) -> String {
+      let inputData = Data(input.utf8)
+      let hashedData = SHA256.hash(data: inputData)
+      let hashString = hashedData.compactMap {
+        String(format: "%02x", $0)
+      }.joined()
+
+      return hashString
+    }
+
+    
+    
+    @available(iOS 13, *)
+    private func startSignInWithAppleFlow() {
+        let nonce = randomNonceString()
+        currentNonce = nonce
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.email]
+        request.nonce = sha256(nonce)
+
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+    
+    
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+            guard let window = UIApplication.shared.delegate?.window else {
+                fatalError()
+            }
+            return window!
+        }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+            
+            if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+                guard let nonce = currentNonce else {
+                    fatalError("Invalid state: A login callback was received, but no login request was sent.")
+                }
+                guard let appleIDToken = appleIDCredential.identityToken else {
+                    print("Unable to fetch identity token")
+                    return
+                }
+                guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                    print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+                    return
+                }
+                let credential = OAuthProvider.credential(
+                    withProviderID: "apple.com",
+                    idToken: idTokenString,
+                    rawNonce: nonce
+                )
+                Auth.auth().signIn(with: credential) { (authResult, error) in
+                    if let error = error {
+                        print(error.localizedDescription)
+                        return
+                    }
+                    print("成功!!!")
+                    let storyBoard = UIStoryboard(name: "BaseTabBar", bundle: nil)
+                    let vc = storyBoard.instantiateViewController(identifier: "baseTab") as! UITabBarController
+                    vc.selectedIndex = 0
+                    self.present(vc, animated: false, completion: nil)
+                }
+            }
+        }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+            print(error.localizedDescription)
+        }
+}
+
+
+
+
+
+
 
 
 extension InitialViewController: UITextViewDelegate {
