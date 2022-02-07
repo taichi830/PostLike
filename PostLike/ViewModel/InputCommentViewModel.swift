e8e9820ab971477106642d3177606e5cf27dbb98
taichi <taicloud0803@icloud.com>
2022-02-07 19:38:00 +0900

    コメントできない不具合を修正

--- PostLike/ViewModel/InputCommentViewModel.swift
+++ PostLike/ViewModel/InputCommentViewModel.swift
@@ -17,8 +17,9 @@ final class InputCommentViewModel {
     var validPostDriver:Driver<Bool> = Driver.never()
     var userInfoDriver: Driver<Contents> = Driver.never()
     var isJoined: Driver<Bool> = Driver.never()
+    let isPosted: Driver<Bool>
     
-    init(input:(postButtonTap:Signal<()>,commentText:Driver<String>),userListner:UserListner,roomID:String) {
+    init(input:(postButtonTap:Signal<()>,commentText:Driver<String>),postComment:PostComment,userListner:UserListner,roomID: String, postID: String, roomName: String, passedUid: String, mediaArray: [String]) {
         
         //投稿ボタンのバリデーション
         validPostDriver = input.commentText.asObservable()
@@ -38,6 +39,18 @@ final class InputCommentViewModel {
         userInfoDriver = userListner
             .debounce(.milliseconds(1000), scheduler: MainScheduler.instance)
             .asDriver(onErrorJustReturn: Contents(dic: ["isJoined" : false]))
+        
+        
+        
+        //投稿処理
+        let combineLatest = Observable.combineLatest(input.commentText.asObservable(), userListner)
+        isPosted = input.postButtonTap
+            .asObservable()
+            .withLatestFrom(combineLatest)
+            .flatMapLatest({ (text,userInfo) -> Single<Bool> in
+                return postComment.postComment(userName: userInfo.userName, userImage: userInfo.userImage, text: text, roomID: roomID, postID: postID, roomName: roomName, passedUid: passedUid, mediaArray: mediaArray)
+            })
+            .asDriver(onErrorJustReturn: false)
         
         
     }
