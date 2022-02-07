2a890f37c1e63b715d124df1441beb28b78186fc
taichi <taicloud0803@icloud.com>
2022-02-07 17:06:55 +0900

    プロフィール取得処理を記述

--- PostLike/Model/UserListner.swift
+++ PostLike/Model/UserListner.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+import Firebase
+
+protocol UserListner {
+    func createUserListner(roomID:String) -> Observable<Contents>
+}
+
+final class UserDefaultLisner: UserListner {
+    private var listner: ListenerRegistration?
+    func createUserListner(roomID: String) -> Observable<Contents> {
+        return.create { observer in
+            let uid = Auth.auth().currentUser!.uid
+            let db = Firestore.firestore()
+            self.listner = db.collection("users").document(uid).collection("rooms").document(roomID).addSnapshotListener({ snapshot, err in
+                
+                if let err = err {
+                    observer.onError(err)
+                    return
+                }
+                guard let snapshot = snapshot, let dic = snapshot.data() else { return }
+                let content = Contents.init(dic: dic)
+                observer.onNext(content)
+            })
+            return Disposables.create {
+                self.listner?.remove()
+            }
+        }
+        
+    }
+    
+    
+}
