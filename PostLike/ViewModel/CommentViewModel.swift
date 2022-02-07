6620496708b37be78d0b3270954401bf85e983cc
taichi <taicloud0803@icloud.com>
2022-02-08 01:29:07 +0900

    itemsの空チェックを記述

--- PostLike/ViewModel/CommentViewModel.swift
+++ PostLike/ViewModel/CommentViewModel.swift
@@ -13,10 +13,20 @@ import Firebase
 
 final class CommentViewModel {
     let items: Driver<[Contents]>
+    let isEmpty: Driver<Bool>
     
     init (commentListner: CommentListner,documentID:String) {
-        items = commentListner.createListner(documentID: documentID)
+        
+        let listner = commentListner.createListner(documentID: documentID)
+        
+        items = listner
             .debounce(.milliseconds(1000), scheduler: MainScheduler.instance)
             .asDriver(onErrorJustReturn: [])
+        
+        isEmpty = listner.map { contents -> Bool in
+            return contents.isEmpty
+        }
+        .asDriver(onErrorJustReturn: true)
+        
     }
 }
