4051400c45f464b32efb000484e822aba7b0289b
taichi <taicloud0803@icloud.com>
2022-02-09 00:56:34 +0900

    InputCommentViewModelのテストを記述

--- PostLikeTests/InputCommentViewModelTests.swift
+++ PostLikeTests/InputCommentViewModelTests.swift
@@ -7,27 +7,72 @@
 //
 
 import XCTest
+import RxSwift
+import RxCocoa
+import RxTest
+@testable import PostLike_Dev
 
 class InputCommentViewModelTests: XCTestCase {
-
+    
+    let disposeBag = DisposeBag()
+    let sheduler = TestScheduler(initialClock: 0)
+    var commentEvent: Driver<String>!
+    var postButtonEvent: Signal<()>!
+    var profileInfo: Observable<Contents>!
+    var viewModel: InputCommentViewModel!
+    
+    
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        //コメント入力イベント
+        commentEvent = sheduler.createHotObservable([
+            .next(0, ""),
+            .next(10, "hello"),
+            .next(20, "")
+        ])
+        .asDriver(onErrorDriveWith: Driver.empty())
+        
+        //投稿ボタンのタップイベント
+        postButtonEvent = sheduler.createHotObservable([
+            .next(0, ()),
+            .next(10, ()),
+            .next(20, ()),
+        ])
+        .asSignal(onErrorSignalWith: Signal.empty())
+        
+        //viewModelを初期化
+        viewModel = InputCommentViewModel(input: (postButtonTap: postButtonEvent, commentText: commentEvent), postComment: PostCommentAPI(), userListner: UserDefaultLisner(), roomID: "LpDA3GOy9H2GIqP9cIOH", postID: "", roomName: "", passedUid: "", mediaArray: [""])
+        
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    //コメント投稿時のバリデーション
+    func testPostCommentValidation() throws {
+        
+        let isPostable = sheduler.createObserver(Bool.self)
+        viewModel.validPostDriver.drive(isPostable).disposed(by: disposeBag)
+        
+        sheduler.start()
+        
+        XCTAssertEqual(isPostable.events, [
+            .next(0, false),
+            .next(10, true),
+            .next(20, false)
+        ])
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    //プロフィール情報の取得テスト
+    func testFetchProfileInfo() {
+        
+//        let userInfo = sheduler.createObserver(Contents.self)
+//        viewModel.userInfoDriver.drive(userInfo).disposed(by: disposeBag)
+//        
+//        let isJoined = sheduler.createObserver(Bool.self)
+//        viewModel.isJoined.drive(isJoined).disposed(by: disposeBag)
+//        
+//        sheduler.start()
+//        
+        
+        
     }
 
 }
