2ca1241219cf80e049f38d163993a1bf0edb9f25
taichi <taicloud0803@icloud.com>
2022-02-10 00:21:31 +0900

    NotificationTableViewCellを作成

---
+++ PostLike/View/Cells/NotificationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NotificationTableViewCell.swift
+//  PostLike
+//
+//  Created by taichi on 2022/02/10.
+//  Copyright © 2022 taichi. All rights reserved.
+//
+
+import UIKit
+
+class NotificationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
