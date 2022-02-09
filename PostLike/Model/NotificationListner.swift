4fa59384e24b68c59c01a36e5fc3977e5312e762
taichi <taicloud0803@icloud.com>
2022-02-10 00:18:26 +0900

    NotificationListner.swift,NotificationViewModel.swiftを作成

---
+++ PostLike/Model/NotificationListner.swift
@@ -0,0 +1,9 @@
+//
+//  NotificationListner.swift
+//  PostLike
+//
+//  Created by taichi on 2022/02/09.
+//  Copyright © 2022 taichi. All rights reserved.
+//
+
+import Foundation
---
+++ PostLike/ViewModel/NotificationViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  NotificationViewModel.swift
+//  PostLike
+//
+//  Created by taichi on 2022/02/10.
+//  Copyright © 2022 taichi. All rights reserved.
+//
+
+import Foundation
