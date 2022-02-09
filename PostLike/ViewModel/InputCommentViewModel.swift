b2366412035bdd57a434de81b172d7246a3082dc
taichi <taicloud0803@icloud.com>
2022-02-09 16:52:09 +0900

    エラー通知を記述

--- PostLike/ViewModel/InputCommentViewModel.swift
+++ PostLike/ViewModel/InputCommentViewModel.swift
@@ -14,10 +14,19 @@ import Firebase
 final class InputCommentViewModel {
     
     private let dispoedBag = DisposeBag()
+    
     var validPostDriver:Driver<Bool> = Driver.never()
     var userInfoDriver: Driver<Contents> = Driver.never()
     var isJoined: Driver<Bool>
+    
+    var isPostedSubject = PublishSubject<Bool>()
     let isPosted: Driver<Bool>
+    
+    var postErrorSubject = PublishSubject<Error>()
+    let postError: Driver<Error>
+    
+    
+    
     
     init(input:(postButtonTap:Signal<()>,commentText:Driver<String>),postComment:PostComment,userListner:UserListner,roomID: String, postID: String, roomName: String, passedUid: String, mediaArray: [String]) {
         
@@ -30,12 +39,10 @@ final class InputCommentViewModel {
         
         //プロフィール情報を取得
         let userListner = userListner.createUserListner(roomID: roomID)
-        
         isJoined = userListner.map({ content -> Bool in
             return content.isJoined != false
         })
         .asDriver(onErrorJustReturn: false)
-        
         userInfoDriver = userListner
             .debounce(.milliseconds(1000), scheduler: MainScheduler.instance)
             .asDriver(onErrorJustReturn: Contents(dic: ["isJoined" : false]))
@@ -43,16 +50,26 @@ final class InputCommentViewModel {
         
         
         //投稿処理
+        isPosted = isPostedSubject.asDriver(onErrorJustReturn: false)
+        postError = postErrorSubject.asDriver(onErrorDriveWith: Driver.empty())
         let combineLatest = Observable.combineLatest(input.commentText.asObservable(), userListner)
-        isPosted = input.postButtonTap
+        input.postButtonTap
             .asObservable()
             .withLatestFrom(combineLatest)
             .flatMapLatest({ (text,userInfo) -> Single<Bool> in
                 return postComment.postComment(userName: userInfo.userName, userImage: userInfo.userImage, text: text, roomID: roomID, postID: postID, roomName: roomName, passedUid: passedUid, mediaArray: mediaArray)
             })
-            .asDriver(onErrorJustReturn: false)
-        
-        
+            .subscribe { [weak self] result in
+                switch result {
+                case .next(let bool):
+                    self?.isPostedSubject.onNext(bool)
+                case .error(let err):
+                    self?.postErrorSubject.onNext(err)
+                case .completed:
+                    break
+                }
+            }
+            .disposed(by: dispoedBag)
     }
     
     
