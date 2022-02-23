40262616d845b476fa428a736c33c358435f6a88
taichi <taicloud0803@icloud.com>
2022-02-23 19:50:06 +0900

    投稿取得処理を記述

--- PostLike/ViewModel/FeedViewModel.swift
+++ PostLike/ViewModel/FeedViewModel.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2022 taichi. All rights reserved.
 //
 
-import Foundation
+import RxSwift
+import RxCocoa
+
+final class FeedViewMode {
+    let items: Driver<[Contents]>
+    let isEmpty: Driver<Bool>
+    
+    init(feedContentsListner: FeedContentsListner, roomID: String) {
+        let listner = feedContentsListner.fetchFeedContents(roomID: roomID)
+        
+        items = listner
+            .debounce(.milliseconds(300), scheduler: MainScheduler.instance)
+            .asDriver(onErrorJustReturn: [])
+        
+        isEmpty = listner.map({ contents -> Bool in
+            return contents.isEmpty
+        })
+        .asDriver(onErrorJustReturn: true)
+    }
+    
+}
