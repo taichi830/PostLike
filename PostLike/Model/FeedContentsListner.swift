059fb41b6d4646ffc75fc05661fa5d228e78a303
taichi <taicloud0803@icloud.com>
2022-02-23 19:50:06 +0900

    投稿取得処理を記述

--- PostLike/Model/FeedContentsListner.swift
+++ PostLike/Model/FeedContentsListner.swift
@@ -6,4 +6,38 @@
 //  Copyright © 2022 taichi. All rights reserved.
 //
 
-import Foundation
+import RxSwift
+import RxCocoa
+import FirebaseFirestore
+
+protocol FeedContentsListner {
+    func fetchFeedContents(roomID: String) -> Observable<[Contents]>
+}
+
+final class FeedContentsDefaultListner: FeedContentsListner {
+    private var listner: ListenerRegistration?
+    func fetchFeedContents(roomID: String) -> Observable<[Contents]> {
+        return Observable.create { observer in
+            let db = Firestore.firestore()
+            self.listner = db.collectionGroup("posts").whereField("roomID", isEqualTo: roomID).order(by: "createdAt", descending: true).limit(to: 10).addSnapshotListener({ querySnapshot, err in
+                if let err = err {
+                    observer.onError(err)
+                    return
+                }
+                guard let querySnapshot = querySnapshot else { return }
+                let contents = querySnapshot.documents.map { snapshot -> Contents in
+                    let dic = snapshot.data()
+                    let contents = Contents(dic: dic)
+                    return contents
+                }
+                observer.onNext(contents)
+            })
+            return Disposables.create {
+                self.listner?.remove()
+            }
+        }
+    }
+    
+    
+    
+}
