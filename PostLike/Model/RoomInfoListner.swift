75838b4dcaa2e68839518e409c1f7181adfbb662
taichi <taicloud0803@icloud.com>
2022-02-25 01:33:08 +0900

    ルーム情報取得処理を記述

--- PostLike/Model/RoomInfoListner.swift
+++ PostLike/Model/RoomInfoListner.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2022 taichi. All rights reserved.
 //
 
-import Foundation
+import RxSwift
+import RxCocoa
+import FirebaseFirestore
+
+protocol RoomInfoListner {
+    func fetchRoomInfo(roomID: String) -> Observable<Room>
+}
+
+final class RoomInfoDefaultListner: RoomInfoListner {
+    private var listner: ListenerRegistration?
+    func fetchRoomInfo(roomID: String) -> Observable<Room> {
+        return Observable.create { observer in
+            let db = Firestore.firestore()
+            self.listner = db.collection("rooms").document(roomID).addSnapshotListener({ snapshot, err in
+                if let err = err {
+                    observer.onError(err)
+                    return
+                }
+                guard let dic = snapshot?.data() else { return }
+                let room = Room(dic: dic)
+                observer.onNext(room)
+            })
+            return Disposables.create {
+                self.listner?.remove()
+            }
+        }
+    }
+    
+    
+}
