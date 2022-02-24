016346982eb19ae0d661480f52d955c83db0ecc6
taichi <taicloud0803@icloud.com>
2022-02-24 23:06:15 +0900

    commit

--- PostLike/Model/LikeListner.swift
+++ PostLike/Model/LikeListner.swift
@@ -6,4 +6,44 @@
 //  Copyright © 2022 taichi. All rights reserved.
 //
 
-import Foundation
+import RxSwift
+import RxCocoa
+import FirebaseFirestore
+import FirebaseAuth
+
+protocol LikeListner {
+    func fetchLikes(contents: [Contents]) -> Observable<[Contents]>
+}
+
+final class LikeDefaultListner: LikeListner {
+    private var listner: ListenerRegistration?
+    func fetchLikes(contents: [Contents]) -> Observable<[Contents]> {
+        return Observable.create { observer in
+            let db = Firestore.firestore()
+            let uid = Auth.auth().currentUser!.uid
+            let documentIDs = contents.map { Contents -> String in
+                return Contents.documentID
+            }
+            self.listner = db.collection("users").document(uid).collection("likes").whereField("documentID", in: documentIDs).limit(to: 10).addSnapshotListener { querySnapshot, err in
+                if let err = err {
+                    observer.onError(err)
+                    return
+                }
+                guard let querySnapshot = querySnapshot else { return }
+                let likes = querySnapshot.documents.map { snapshot -> Contents in
+                    let dic = snapshot.data()
+                    let like = Contents(dic: dic)
+                    return like
+                }
+                observer.onNext(likes)
+                
+            }
+            
+            return Disposables.create {
+                self.listner?.remove()
+            }
+        }
+    }
+    
+    
+}
--- PostLike/ViewModel/RoomHeaderViewModel.swift
+++ PostLike/ViewModel/RoomHeaderViewModel.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2022 taichi. All rights reserved.
 //
 
-import Foundation
+import RxSwift
+import RxCocoa
+import Firebase
+
+final class RoomHeaderViewModel {
+    private let disposeBag = DisposeBag()
+    let userInfo: Driver<Contents>
+    let isJoined: Driver<Bool>
+    
+    init(userListner: UserListner, roomID: String) {
+        let listner = userListner.createUserListner(roomID: roomID)
+        
+        userInfo = listner.debounce(.milliseconds(100), scheduler: MainScheduler.instance)
+            .asDriver(onErrorDriveWith: Driver.empty())
+        
+        isJoined = userInfo.asObservable()
+            .map { content -> Bool in
+                return content.isJoined
+            }
+            .asDriver(onErrorJustReturn: false)
+    }
+}
+
+
