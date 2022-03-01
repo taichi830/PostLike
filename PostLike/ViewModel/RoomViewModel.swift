feb7171d23345de5ba24dbb92de2504aaf34ef07
taichi <taicloud0803@icloud.com>
2022-03-02 00:59:18 +0900

    参加中のルームを作成

--- PostLike/ViewModel/RoomViewModel.swift
+++ PostLike/ViewModel/RoomViewModel.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2022 taichi. All rights reserved.
 //
 
-import Foundation
+import RxSwift
+import RxCocoa
+
+final class RoomViewModel {
+    let rooms: Driver<[Contents]>
+    let isEmpty: Driver<Bool>
+    
+    init(roomListner: RoomListner) {
+        
+        rooms = roomListner.fetchRooms()
+            .debounce(.milliseconds(50), scheduler: MainScheduler.instance)
+            .asDriver(onErrorJustReturn: [])
+        
+        isEmpty = rooms.compactMap{ rooms in
+            return rooms.isEmpty
+        }
+        .asDriver(onErrorJustReturn: true)
+    }
+    
+}
