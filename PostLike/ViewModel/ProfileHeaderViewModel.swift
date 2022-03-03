0203e7086708d5df6d1a0c4510eb13d18b9f9888
taichi <taicloud0803@icloud.com>
2022-03-04 02:43:58 +0900

    ProfileHeaderViewをrxで書き換え

--- PostLike/ViewModel/ProfileHeaderViewModel.swift
+++ PostLike/ViewModel/ProfileHeaderViewModel.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2022 taichi. All rights reserved.
 //
 
-import Foundation
+import RxSwift
+import RxCocoa
+
+final class ProfileHeaderViewModel {
+    let userInfo: Driver<Contents>
+    let postCount: Driver<Contents>
+    let likeCount: Driver<Contents>
+    
+    init(userListner: UserListner, roomID: String) {
+        userInfo = userListner.createUserListner(roomID: roomID)
+            .debounce(.milliseconds(50), scheduler: MainScheduler.instance)
+            .asDriver(onErrorDriveWith: Driver.empty())
+        
+        postCount = userListner.fetchPostCount(roomID: roomID)
+            .debounce(.milliseconds(50), scheduler: MainScheduler.instance)
+            .asDriver(onErrorDriveWith: Driver.empty())
+        
+        likeCount = userListner.fetchLikeCount(roomID: roomID)
+            .debounce(.milliseconds(50), scheduler: MainScheduler.instance)
+            .asDriver(onErrorDriveWith: Driver.empty())
+    }
+}
