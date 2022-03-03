2a9725f041e16c6f834fc2f73876c0c9beae49ae
taichi <taicloud0803@icloud.com>
2022-03-04 02:42:52 +0900

    ProfileHeaderViewをrxで書き換え

--- PostLike/Model/UserListner.swift
+++ PostLike/Model/UserListner.swift
@@ -14,9 +14,49 @@ import FirebaseFirestore
 
 protocol UserListner {
     func createUserListner(roomID:String) -> Observable<Contents>
+    func fetchLikeCount(roomID: String) -> Observable<Contents>
+    func fetchPostCount(roomID: String) -> Observable<Contents>
 }
 
 final class UserDefaultLisner: UserListner {
+    private var likeCountListner: ListenerRegistration?
+    func fetchLikeCount(roomID: String) -> Observable<Contents> {
+        return Observable.create { observer in
+            let uid = Auth.auth().currentUser!.uid
+            let db = Firestore.firestore()
+            self.likeCountListner = db.collection("users").document(uid).collection("rooms").document(roomID).collection("profileLikeCount").document("count").addSnapshotListener { snapshot, err in
+                if let err = err {
+                    print("err:", err)
+                    observer.onError(err)
+                    return
+                }
+                guard let snapshot = snapshot, let dic = snapshot.data() else { return }
+                let content = Contents(dic: dic)
+                observer.onNext(content)
+            }
+            return Disposables.create()
+        }
+    }
+    
+    private var postCountListner: ListenerRegistration?
+    func fetchPostCount(roomID: String) -> Observable<Contents> {
+        return Observable.create { observer in
+            let uid = Auth.auth().currentUser!.uid
+            let db = Firestore.firestore()
+            self.likeCountListner = db.collection("users").document(uid).collection("rooms").document(roomID).collection("profilePostCount").document("count").addSnapshotListener { snapshot, err in
+                if let err = err {
+                    print("err:", err)
+                    observer.onError(err)
+                    return
+                }
+                guard let snapshot = snapshot, let dic = snapshot.data() else { return }
+                let content = Contents(dic: dic)
+                observer.onNext(content)
+            }
+            return Disposables.create()
+        }
+    }
+    
     private var listner: ListenerRegistration?
     func createUserListner(roomID: String) -> Observable<Contents> {
         return Observable.create { observer in
