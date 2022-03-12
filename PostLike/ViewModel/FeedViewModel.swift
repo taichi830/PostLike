0020122b7b80f17b82af382194c0e346fb647bfc
taichi <taicloud0803@icloud.com>
2022-03-12 14:44:45 +0900

    EnteredRoomContentVCの更新機能を実装

--- PostLike/ViewModel/FeedViewModel.swift
+++ PostLike/ViewModel/FeedViewModel.swift
@@ -23,11 +23,17 @@ final class FeedViewModel {
     var likesRelay = BehaviorRelay<[Contents]>.init(value: [])
     var likes: Driver<[Contents]> = Driver.never()
     
+    var isEmptyRelay = PublishRelay<Bool>()
     var isEmpty: Driver<Bool> = Driver.never()
     
     var isBottomSubject = BehaviorSubject<Bool>.init(value: false)
     var isBottomObserver: AnyObserver<Bool> {
         isBottomSubject.asObserver()
+    }
+    
+    var refreshSubject = PublishSubject<()>()
+    var refreshObserver: AnyObserver<()> {
+        refreshSubject.asObserver()
     }
     
     
@@ -36,45 +42,113 @@ final class FeedViewModel {
         
         items = itemsRelay.asDriver(onErrorJustReturn: [])
         likes = likesRelay.asDriver(onErrorJustReturn: [])
-//        isLoading = isLoadingRelay.asDriver()
+        isEmpty = isEmptyRelay.asDriver(onErrorJustReturn: true)
+        
+        
+        
+        
         
         
         
         let fetchPosts = feedContentsListner.fetchPosts(roomID: roomID)
             .share(replay: 1)
+        self.isEmptyCheck(fetchObservable: fetchPosts)
+        self.fetchLikes(fetchObservable: fetchPosts, likeListner: likeListner, currentLikes: [])
+        self.fetchMoreContents(fetchObservable: fetchPosts, reportListner: reportListner, currentItems: [])
         
-        //空チェック
-        isEmpty = fetchPosts.asObservable()
-            .map { items -> Bool in
-                return items.isEmpty
+        
+        //更新通知を受け取る
+        refreshSubject.asObservable()
+            .subscribe { [weak self] _ in
+                let fetchPosts = feedContentsListner.fetchPosts(roomID: roomID)
+                    .share(replay: 1)
+                self?.isEmptyCheck(fetchObservable: fetchPosts)
+                self?.fetchLikes(fetchObservable: fetchPosts, likeListner: likeListner, currentLikes: [])
+                self?.fetchMoreContents(fetchObservable: fetchPosts, reportListner: reportListner, currentItems: [])
             }
-            .asDriver(onErrorJustReturn: true)
+            .disposed(by: disposeBag)
         
         
-//        fetchPosts
-//            .subscribe { [weak self] contents in
-//                guard let element = contents.element else { return }
-//                self?.feedContentsRelay.accept(element)
+//        //報告したユーザーを取得
+//        let reportedUsers = fetchPosts.asObservable()
+//            .filter{ !$0.isEmpty }
+//            .concatMap { contents -> Observable<[Contents]> in
+//                return reportListner.fetchReportedUsers(contents: contents)
+//            }
+//
+//        //報告した投稿を取得
+//        let reportedContents = fetchPosts.asObservable()
+//            .filter{ !$0.isEmpty }
+//            .concatMap { contents -> Observable<[Contents]> in
+//                return reportListner.fetchReportedContents(contents: contents)
+//            }
+//
+//        //報告した投稿をremoveする
+//        Observable.combineLatest(fetchPosts, reportedUsers, reportedContents)
+//            .subscribe { (items,users,contents) in
+//                let filteredItems = items.filter { item -> Bool in
+//                    !users.contains(where: { user in
+//                        item.uid == user.uid
+//                    }) &&
+//                    !contents.contains(where: { content in
+//                        item.documentID == content.documentID
+//                    })
+//                }
+//                self.itemsRelay.accept(filteredItems)
+//            }
+//            .disposed(by: disposeBag)
+//
+//
+//        //いいねした投稿を取得
+//        fetchPosts.asObservable()
+//            .filter{ !$0.isEmpty }
+//            .concatMap { contents -> Observable<[Contents]> in
+//                return likeListner.fetchLikes(contents: contents)
+//            }
+//            .subscribe { [weak self] likes in
+//                self?.likesRelay.accept(likes)
 //            }
 //            .disposed(by: disposeBag)
         
         
+        
+        
+        isBottomSubject.asObservable()
+            .subscribe { [weak self] _ in
+                let fetchMorePosts = feedContentsListner.fetchMorePosts(roomID: roomID)
+                    .share(replay: 1)
+                let currentItems = self?.itemsRelay.value ?? []
+                let currentLikes = self?.likesRelay.value ?? []
+                self?.fetchLikes(fetchObservable: fetchMorePosts, likeListner: likeListner, currentLikes: currentLikes)
+                self?.fetchMoreContents(fetchObservable: fetchMorePosts, reportListner: reportListner, currentItems: currentItems)
+            }
+            .disposed(by: disposeBag)
+        
+        
+    
+    }
+    
+    
+    
+    
+    private func fetchMoreContents(fetchObservable: Observable<[Contents]>, reportListner: ReportListner, currentItems: [Contents]) {
+        
         //報告したユーザーを取得
-        let reportedUsers = fetchPosts.asObservable()
-            .filter{ !$0.isEmpty }
+        let reportedUsers = fetchObservable.asObservable()
+            .filter { !$0.isEmpty }
             .concatMap { contents -> Observable<[Contents]> in
                 return reportListner.fetchReportedUsers(contents: contents)
             }
         
         //報告した投稿を取得
-        let reportedContents = fetchPosts.asObservable()
-            .filter{ !$0.isEmpty }
+        let reportedContents = fetchObservable.asObservable()
+            .filter { !$0.isEmpty }
             .concatMap { contents -> Observable<[Contents]> in
                 return reportListner.fetchReportedContents(contents: contents)
             }
         
         //報告した投稿をremoveする
-        Observable.combineLatest(fetchPosts, reportedUsers, reportedContents)
+        Observable.combineLatest(fetchObservable, reportedUsers, reportedContents)
             .subscribe { (items,users,contents) in
                 let filteredItems = items.filter { item -> Bool in
                     !users.contains(where: { user in
@@ -84,47 +158,16 @@ final class FeedViewModel {
                         item.documentID == content.documentID
                     })
                 }
-                self.itemsRelay.accept(filteredItems)
+                self.itemsRelay.accept(currentItems + filteredItems)
             }
             .disposed(by: disposeBag)
-        
-        
-        //いいねした投稿を取得
-        fetchPosts.asObservable()
-            .filter{ !$0.isEmpty }
-            .concatMap { contents -> Observable<[Contents]> in
-                return likeListner.fetchLikes(contents: contents)
-            }
-            .subscribe { [weak self] likes in
-                self?.likesRelay.accept(likes)
-            }
-            .disposed(by: disposeBag)
-        
-        
-        
-        
-        isBottomSubject.asObservable()
-            .subscribe { [weak self] _ in
-                self?.fetchMoreContents(feedListner: feedContentsListner, likeListner: likeListner, reportListner: reportListner, roomID: roomID)
-            }
-            .disposed(by: disposeBag)
-        
-        
-    
     }
     
     
     
-    
-    private func fetchMoreContents(feedListner: FeedContentsListner, likeListner: LikeListner, reportListner: ReportListner, roomID: String) {
-        
-        let currentContents = self.itemsRelay.value
-        let currentLikes = self.likesRelay.value
-        let fetchMorePosts = feedListner.fetchMorePosts(roomID: roomID)
-            .share(replay: 1)
-        
+    private func fetchLikes(fetchObservable: Observable<[Contents]>, likeListner: LikeListner, currentLikes: [Contents]) {
         //いいねした投稿を取得
-        fetchMorePosts.asObservable()
+        fetchObservable.asObservable()
             .filter { !$0.isEmpty }
             .concatMap { contents -> Observable<[Contents]> in
                 return likeListner.fetchLikes(contents: contents)
@@ -134,38 +177,18 @@ final class FeedViewModel {
                 self?.likesRelay.accept(currentLikes + likes)
             }
             .disposed(by: disposeBag)
-        
-        //報告したユーザーを取得
-        let reportedUsers = fetchMorePosts.asObservable()
-            .filter { !$0.isEmpty }
-            .concatMap { contents -> Observable<[Contents]> in
-                return reportListner.fetchReportedUsers(contents: contents)
-            }
-        
-        //報告した投稿を取得
-        let reportedContents = fetchMorePosts.asObservable()
-            .filter { !$0.isEmpty }
-            .concatMap { contents -> Observable<[Contents]> in
-                return reportListner.fetchReportedContents(contents: contents)
-            }
-        
-        //報告した投稿をremoveする
-        Observable.combineLatest(fetchMorePosts, reportedUsers, reportedContents)
-            .subscribe { (items,users,contents) in
-                let filteredItems = items.filter { item -> Bool in
-                    !users.contains(where: { user in
-                        item.uid == user.uid
-                    }) &&
-                    !contents.contains(where: { content in
-                        item.documentID == content.documentID
-                    })
-                }
-                self.itemsRelay.accept(currentContents + filteredItems)
+    }
+    
+    
+    
+    private func isEmptyCheck(fetchObservable: Observable<[Contents]>) {
+        //空チェック
+        fetchObservable.asObservable()
+            .map { $0.isEmpty }
+            .subscribe { [weak self] bool  in
+                self?.isEmptyRelay.accept(bool)
             }
             .disposed(by: disposeBag)
-        
-        
-        
     }
     
     
