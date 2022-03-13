4a9f038f3be30b041dd2c761f786363a92b5a7a3
taichi <taicloud0803@icloud.com>
2022-03-13 12:42:37 +0900

    スクロールで最下部の近くにきたかを検知

--- PostLike/Extenshion/UIScrollView+Extension.swift
+++ PostLike/Extenshion/UIScrollView+Extension.swift
@@ -6,4 +6,13 @@
 //  Copyright © 2022 taichi. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIScrollView {
+    //スクロールで最下部の近くにきたかを検知
+    func isNearBottomEdge() -> Bool {
+        let edgeOffset: CGFloat = 20
+        return contentOffset.y + frame.size.height + edgeOffset > contentSize.height
+    }
+    
+}
