7838db18ed88b82bb8eb217f7c88d121e751b2d4
taichi <taicloud0803@icloud.com>
2022-03-17 16:17:32 +0900

    ルームの形を正方形に変更

--- PostLike/View/Cells/SearchResultTableViewCell.swift
+++ PostLike/View/Cells/SearchResultTableViewCell.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class SearchResultTableViewCell: UITableViewCell {
-
+    
+    
+    @IBOutlet private weak var roomImageView: UIImageView! {
+        didSet {
+            roomImageView.layer.cornerRadius = 10
+        }
+    }
+    
+    @IBOutlet private weak var roomNameLabel: UILabel!
+    
+    @IBOutlet private weak var roomIntroLabel: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -19,6 +31,19 @@ class SearchResultTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func setupCell(roomInfo: Room) {
+        roomImageView.setImage(imageUrl: roomInfo.roomImage)
+        roomNameLabel.text = roomInfo.roomName
+        roomIntroLabel.text = roomInfo.roomIntro
+        if roomInfo.roomIntro == "" {
+            roomIntroLabel.textColor = .lightGray
+            roomIntroLabel.text = "紹介文はありません"
+        }else{
+            roomIntroLabel.textColor = .black
+            roomIntroLabel.text = roomInfo.roomIntro
+        }
     }
     
 }
