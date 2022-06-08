68529bd1c6c2f4879bc303cc4250ed36ae4221b9
taichi <taicloud0803@icloud.com>
2022-06-08 17:26:41 +0900

    コードを整理しました

--- PostLike/ViewModel/PostViewModel.swift
+++ PostLike/ViewModel/PostViewModel.swift
@@ -35,18 +35,26 @@ final class PostViewModel: PostViewModelType, PostViewModelInputs, PostViewModel
     var inputs: PostViewModelInputs { return self }
     var outputs: PostViewModelOutputs { return self }
     
-    //MARK: - Inputs
+    // Inputs
     var text = BehaviorRelay<String>.init(value: "")
     var photos = BehaviorRelay<[UIImage]>.init(value: [])
     
-    //MARK: - Outputs
+    // Outputs
     var isPostButtonEnabled: Driver<Bool>
     var isAlbumButtonEnabled: Driver<Bool>
     var isPosted: Driver<Bool> = Driver.never()
     var postError: Driver<Error> = Driver.never()
-    var imageCountDriver:Driver<Int>
+    var imageCountDriver: Driver<Int>
     var outputPhotos: Driver<[UIImage]>
     
+    // Private Properties
+    private var postButtonTap: Signal<()>
+    private var albumButtonTap: Signal<()>
+    private var userName: String
+    private var userImage: String
+    private var passedUid: String
+    private var roomID: String
+    private var postAPI: PostAPI
     private let disposeBag = DisposeBag()
     
     
@@ -54,8 +62,16 @@ final class PostViewModel: PostViewModelType, PostViewModelInputs, PostViewModel
     
     init(input:(postButtonTap:Signal<()>,albumButtonTap:Signal<()>),userName:String,userImage:String,passedUid:String,roomID:String, postAPI: PostAPI) {
         
+        self.postButtonTap = input.postButtonTap
+        self.albumButtonTap = input.albumButtonTap
+        self.userName = userName
+        self.userImage = userImage
+        self.passedUid = passedUid
+        self.roomID = roomID
+        self.postAPI = postAPI
         
-        //テキストまたは写真があれば投稿ボタンを有効にする
+        
+        // テキストまたは写真があれば投稿ボタンを有効にする
         isPostButtonEnabled = Observable.combineLatest(text, photos)
             .map { (text, photos)  in
                 return text != "" || !photos.isEmpty
@@ -63,7 +79,7 @@ final class PostViewModel: PostViewModelType, PostViewModelInputs, PostViewModel
             .asDriver(onErrorDriveWith: .empty())
         
         
-        //写真が２個より少なければアルバムボタンを有効にする
+        // 写真が２個より少なければアルバムボタンを有効にする
         isAlbumButtonEnabled = photos.asObservable()
             .map { photos in
                 return photos.count < 2
@@ -71,7 +87,7 @@ final class PostViewModel: PostViewModelType, PostViewModelInputs, PostViewModel
             .asDriver(onErrorDriveWith: .empty())
         
         
-        //アルバムボタンが押されるたび現在の写真の数を通知する
+        // アルバムボタンが押されるたび現在の写真の数を通知する
         imageCountDriver = input.albumButtonTap.asObservable()
             .withLatestFrom(photos.asObservable())
             .map { photos in
@@ -80,12 +96,12 @@ final class PostViewModel: PostViewModelType, PostViewModelInputs, PostViewModel
             .asDriver(onErrorDriveWith: .empty())
         
         
-        //写真をアウトプット
+        // 写真をアウトプット
         outputPhotos = photos.asDriver(onErrorJustReturn: [])
         
         
-        //投稿処理
-        post(postButtonTap: input.postButtonTap, userName: userName, userImage: userImage, passedUid: passedUid, roomID: roomID, postAPI: postAPI)
+        // 投稿処理
+        post()
         
         
     }
@@ -94,14 +110,14 @@ final class PostViewModel: PostViewModelType, PostViewModelInputs, PostViewModel
     
     
     
-    func post(postButtonTap:Signal<()>,userName:String,userImage:String,passedUid:String,roomID:String, postAPI: PostAPI) {
+    func post() {
         let combinedObservable = Observable.combineLatest(text.asObservable(), photos.asObservable())
         
         let result = postButtonTap
             .asObservable()
             .withLatestFrom(combinedObservable)
             .flatMapLatest { (text, photos) -> Observable<Bool> in
-                return postAPI.post(userName: userName, userImage: userImage, text: text, passedUid: passedUid, roomID: roomID, imageArray: photos)
+                return self.postAPI.post(userName: self.userName, userImage: self.userImage, text: text, passedUid: self.passedUid, roomID: self.roomID, imageArray: photos)
             }
             .materialize()
             .share(replay: 1)
@@ -121,7 +137,7 @@ final class PostViewModel: PostViewModelType, PostViewModelInputs, PostViewModel
     
     
     
-    func fetchMyLatestPost(feedListner: GetPosts ,roomID:String) {
+    func fetchMyLatestPost(feedListner: GetPosts, roomID: String) {
         feedListner.fetchMyLatestPost(roomID: roomID)
             .debounce(.milliseconds(50), scheduler: MainScheduler.instance)
             .subscribe{ content in
@@ -136,7 +152,7 @@ final class PostViewModel: PostViewModelType, PostViewModelInputs, PostViewModel
     
     
     
-    func remove(row:Int) {
+    func remove(row: Int) {
         var items = photos.value
         items.remove(at: row)
         photos.accept(items)
